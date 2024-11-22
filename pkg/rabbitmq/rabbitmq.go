6a24cb6f965cfdd45201bfa64e87722edbe5b4e1
aparnasukesh <aparnasukesh0723@gmail.com>
2024-11-22 12:03:16 +0530

    rabbitmq connection updated

--- pkg/rabbitmq/rabbitmq.go
+++ pkg/rabbitmq/rabbitmq.go
@@ -4,6 +4,10 @@ import (
 	"github.com/streadway/amqp"
 )
 
+// func NewRabbitMQConnection() (*amqp.Connection, error) {
+// 	return amqp.Dial("amqp://guest:guest@localhost:5672/")
+// }
+
 func NewRabbitMQConnection() (*amqp.Connection, error) {
-	return amqp.Dial("amqp://guest:guest@localhost:5672/")
+	return amqp.Dial("amqp://user:password@rabbitmq:5672/") // Use updated credentials
 }
